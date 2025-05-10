8cab610dfaa2bc2c82d060fc3d61474fe6327583
artur10563 <galkaartur2004galkaartur@gmail.com>
2025-05-11 00:24:13 +0300

    fix

--- BlockchainSQL/Extensions/DependencyInjectionExtension.cs
+++ BlockchainSQL/Extensions/DependencyInjectionExtension.cs
@@ -1,18 +1,17 @@
 ﻿using BlockchainSQL.Helpers;
 using BlockchainSQL.Interceptors;
 using BlockchainSQL.Services.BlockChainService;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
-namespace BlockchainSQL.Extensions;
 
 public static class DependencyInjectionExtension
 {
-    public static IServiceCollection AddBlockchainSql(this IServiceCollection services)
+    public static IServiceCollection AddBlockchainSql(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddScoped<IBlockChainService, BlockChainService>();
+        services.Configure<BlockchainOptions>(configuration.GetSection("Blockchain"));
+        services.AddScoped<IBlockChainService, BlockchainSQL.Services.BlockChainService.BlockChainService>();
         services.AddScoped<QueryValidatorHelper>();
         services.AddScoped<DbQueryInterceptor>();
-
         return services;
     }
 }
